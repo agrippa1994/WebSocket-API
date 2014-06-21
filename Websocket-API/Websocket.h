778892846abf71c8eb736b080bf2307f7cdeac62
agrippa1994 <agrippa1994@icloud.com>
2014-06-21 21:07:09 +0200

    Websocket interface

---
+++ Websocket-API/Websocket.h
@@ -0,0 +1,11 @@
+#define _WEBSOCKETPP_CPP11_THREAD_
+#define _WEBSOCKETPP_CPP11_FUNCTIONAL_
+#define _WEBSOCKETPP_CPP11_SYSTEM_ERROR_
+#define _WEBSOCKETPP_CPP11_RANDOM_DEVICE_
+#define _WEBSOCKETPP_CPP11_MEMORY_
+
+#include <websocketpp/config/asio_no_tls.hpp>
+#include <websocketpp/client.hpp>
+
+typedef websocketpp::client<websocketpp::config::asio> websocket_client;
+typedef websocket_client::message_ptr websocket_message;
